8449066e5cf1b1942a08af50e8946f3081353021
Cristian Gabriel Fiorotto <43812955+9cero@users.noreply.github.com>
2026-04-03 06:57:52 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'ar-titulares-v1'
+const ARCHIVOS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  )
+})
